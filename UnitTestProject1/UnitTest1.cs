790c058569ba87eea6a145c57c96d5e6f47b4c4d
Firesgame <slavaboll@gmail.com>
2024-03-28 21:37:05 +0500

    check

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MusicLibForms;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestValidateNotEmpty()
+		{
+			Assert.IsFalse(Validator.ValidateNotEmpty(" "));
+			Assert.IsTrue(Validator.ValidateNotEmpty("Hello"));
+		}
+
+		[TestMethod]
+		public void TestValidatelogin()
+		{
+			Assert.IsFalse(Validator.Validatelogin("Hello World"));
+			Assert.IsTrue(Validator.Validatelogin("HelloWorld"));
+		}
+
+		[TestMethod]
+		public void TestValidateIntLen()
+		{
+			Assert.IsFalse(Validator.ValidateIntLen("1234", 3));
+			Assert.IsTrue(Validator.ValidateIntLen("123", 3));
+		}
+
+		[TestMethod]
+		public void TestValidateEmail()
+		{
+			Assert.IsFalse(Validator.ValidateEmail("test"));
+			Assert.IsTrue(Validator.ValidateEmail("test@example.com"));
+		}
+
+		[TestMethod]
+		public void SongsCr()
+		{
+			var sc = new SongsCRUD();
+			Assert.IsTrue(true);
+		}
+	}
+}
